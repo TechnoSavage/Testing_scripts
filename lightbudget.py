f42ea717e0b80e6afe8bfe147e31712f188d491f
LinuxFlyGuy <vagrant83@gmail.com>
2017-12-07 14:57:57 -0500

    begin cleaning up and refining script

--- lightbudget.py
+++ lightbudget.py
@@ -7,7 +7,7 @@ from decimal import *
 def menu():
     option = raw_input("""\nWhat would you like to do:
     1 - Calculate the max allowed coupler loss for inserting a TAP into a link
-    2 - Calculate the max allowed cable length for a given TAPped link
+    2 - Calculate the max allowed cable length for a given TAP split ratio in a link
     3 - Exit
     Enter your selection: """)
     try:
@@ -31,23 +31,29 @@ def menu():
 
 def max_split():
     getcontext().prec = 10
-    sender = Decimal(raw_input("\nwhat is the sender transmit power: "))
-    receiver = Decimal(raw_input("\nWhat is the receiver sensitivity: "))
+    sender = Decimal(raw_input("\nwhat is the sender transmit power (dB): "))
+    receiver = Decimal(raw_input("\nWhat is the receiver sensitivity (dB): "))
     link_loss_budget = sender - receiver
-    print "\nThe Power Link Loss Budget for this link is %s dB" % link_loss_budget
-    print """\nSingle Mode or Multi Mode?
+    print "\nThe Power Link Loss Budget for this link is %sdB" % link_loss_budget
+    print """\nSingle Mode or Multi Mode fiber?
     1 - Single Mode
     2 - Multi Mode"""
-    mode = int(raw_input("Enter the number of your selection: "))
-    connectors = Decimal(raw_input("\nHow many connectors total are in the path of the link: "))
-    if mode == 1:
+    mode = raw_input("Enter the number of your selection: ")
+    if mode not in ('1', '2'):
+        print "That is not a valid selection"
+        menu()
+    connectors = Decimal(raw_input("\nHow many connectors are in the path of the link: "))
+    if mode == '1':
         connector_loss = Decimal('0.2') * connectors
         mode_type = "Single Mode"
         print """\nWhat is the wavelength being used?
-        1 - 1310
-        2 - 1550"""
-        wave = int(raw_input("\nEnter the number of your selection: "))
-        if wave == 1:
+        1 - 1310nm
+        2 - 1550nm"""
+        wave = raw_input("\nEnter the number of your selection: ")
+        if wave not in ('1', '2'):
+            print "\nThat is not a valid selection"
+            menu()
+        if wave == '1':
             wavelength = 1310
         else:
             wavelength = 1550
@@ -55,14 +61,17 @@ def max_split():
         connector_loss = Decimal('0.5') * connectors
         mode_type = "Multi Mode"
         print """\nWhat is the wavelength being used?
-        1 - 850
-        2 - 1300"""
-        wave = int(raw_input("Enter the number of your selection: "))
-        if wave == 1:
+        1 - 850nm
+        2 - 1300nm"""
+        wave = raw_input("Enter the number of your selection: ")
+        if wave not in ('1', '2'):
+            print "\nThat is not a valid selection"
+            menu()
+        if wave == '1':
             wavelength = 850
         else:
             wavelength = 1300
-    print "\nThe total loss introduced for the %s link by connectors is %s dB\n" % (mode_type, connector_loss)
+    print "\nThe total loss introduced for the %s link by connectors is %sdB\n" % (mode_type, connector_loss)
     cable = int(raw_input("What is the cable length from the sender to the receiver in meters: "))
     if mode_type == 'Single Mode' and wavelength == 1310:
         attenuation = Decimal('0.4')
@@ -74,18 +83,27 @@ def max_split():
         attenuation = Decimal('1.0')
     else:
         print "Something went wrong."
-    cable_loss = Decimal(cable / 1000.00000) * attenuation
-    print """\nThe loss introduced by the cable length for the %s %s link
-    is %s dB based on %s dB/km fiber attenuation.\n""" % (mode_type, wavelength, cable_loss, attenuation)
+    cable_loss = Decimal(cable / 1000.000) * attenuation
+    print """\nThe loss introduced by the length of cable for the %s %s link
+    is %sdB based on %sdB/km fiber attenuation.\n""" % (mode_type, wavelength, cable_loss, attenuation)
     total_cable_loss = connector_loss + cable_loss
-    print "The total connection loss is %s dB\n" % total_cable_loss
+    print "The total connection loss is %sdB\n" % total_cable_loss
     allowable_loss = link_loss_budget - total_cable_loss
-    print """The allowable coupler loss for a TAP is a %s dB
+    print """The allowable coupler loss for a TAP is a %sdB
     maximum at the monitor port\n""" % allowable_loss
-    loss_match(mode_type, allowable_loss)
+    print """Reference which TAP insertion loss values?
+    1 - Industry Recommended
+    2 - Cubro Average"""
+    choice = raw_input("Enter your selection: ")
+    if choice not in ('1', '2'):
+        print "That is not a valid selection"
+        menu()
+    if choice == '1':
+        match_industry(mode_type, allowable_loss)
+    if choice == '2':
+        match_cubro(mode_type, allowable_loss)
 
-
-def loss_match(mode, loss):
+def match_industry(mode, loss):
     #Maximum recommended values
     taps_mm = {
                '50/50': {'Network': '4.5', 'Monitor': '4.5'},
@@ -101,21 +119,6 @@ def loss_match(mode, loss):
                '80/20': {'Network': '1.3', 'Monitor': '8.0'},
                '90/10': {'Network': '0.8', 'Monitor': '12.0'}
                }
-    #Adjusted average Cubro values
-    # taps_mm = {
-    #            '50/50': {'Network': '4.5', 'Monitor': '4.5'},
-    #            '60/40': {'Network': '3.1', 'Monitor': '5.1'},
-    #            '70/30': {'Network': '2.4', 'Monitor': '6.3'},
-    #            '80/20': {'Network': '1.8', 'Monitor': '8.1'},
-    #            '90/10': {'Network': '1.3', 'Monitor': '11.5'}
-    #            }
-    # taps_sm = {
-    #            '50/50': {'Network': '3.6', 'Monitor': '3.5'},
-    #            '60/40': {'Network': '2.8', 'Monitor': '4.8'},
-    #            '70/30': {'Network': '2.0', 'Monitor': '6.1'},
-    #            '80/20': {'Network': '1.3', 'Monitor': '8.0'},
-    #            '90/10': {'Network': '0.8', 'Monitor': '12.0'}
-    #            }
     usable = []
     if mode == 'Single Mode':
         for split in taps_sm:
@@ -127,29 +130,65 @@ def loss_match(mode, loss):
                 usable.append(split)
     else:
         print "Something went wrong"
-    print """The following split ratios are acceptable for this link
+    print """\nThe following split ratios are acceptable for this link
+    %s""" % usable
+    menu()
+
+def match_cubro(mode, loss):
+    #Adjusted average Cubro values
+    taps_mm = {
+               '50/50': {'Network': '4.5', 'Monitor': '4.5'},
+               '60/40': {'Network': '3.1', 'Monitor': '5.1'},
+               '70/30': {'Network': '2.4', 'Monitor': '6.3'},
+               '80/20': {'Network': '1.8', 'Monitor': '8.1'},
+               '90/10': {'Network': '1.3', 'Monitor': '11.5'}
+               }
+    taps_sm = {
+               '50/50': {'Network': '3.6', 'Monitor': '3.5'},
+               '60/40': {'Network': '2.8', 'Monitor': '4.8'},
+               '70/30': {'Network': '2.0', 'Monitor': '6.1'},
+               '80/20': {'Network': '1.3', 'Monitor': '8.0'},
+               '90/10': {'Network': '0.8', 'Monitor': '12.0'}
+               }
+    usable = []
+    if mode == 'Single Mode':
+        for split in taps_sm:
+            if float(taps_sm[split]['Monitor']) < float(loss):
+                usable.append(split)
+    elif mode == 'Multi Mode':
+        for split in taps_mm:
+            if float(taps_mm[split]['Monitor']) < float(loss):
+                usable.append(split)
+    else:
+        print "Something went wrong"
+    print """\nThe following split ratios are acceptable for this link
     %s""" % usable
     menu()
 
 def max_cable():
     getcontext().prec = 10
-    sender = Decimal(raw_input("\nwhat is the sender transmit power: "))
-    receiver = Decimal(raw_input("\nWhat is the receiver sensitivity: "))
+    sender = Decimal(raw_input("\nwhat is the sender transmit power (dB): "))
+    receiver = Decimal(raw_input("\nWhat is the receiver sensitivity (dB): "))
     link_loss_budget = sender - receiver
-    print "\nThe Power Link Loss Budget for this link is %s dB" % link_loss_budget
-    print """\nSingle Mode or Multi Mode?
+    print "\nThe Power Link Loss Budget for this link is %sdB" % link_loss_budget
+    print """\nSingle Mode or Multi Mode fiber?
     1 - Single Mode
     2 - Multi Mode"""
-    mode = int(raw_input("Enter the number of your selection: "))
-    connectors = Decimal(raw_input("\nHow many connectors total are in the path of the link: "))
-    if mode == 1:
+    mode = raw_input("Enter the number of your selection: ")
+    if mode not in ('1', '2'):
+        print "\nThat is not a valid selection."
+        menu()
+    connectors = Decimal(raw_input("\nHow many connectors are in the path of the link: "))
+    if mode == '1':
         connector_loss = Decimal('0.2') * connectors
         mode_type = "Single Mode"
         print """\nWhat is the wavelength being used?
-        1 - 1310
-        2 - 1550"""
-        wave = int(raw_input("\nEnter the number of your selection: "))
-        if wave == 1:
+        1 - 1310nm
+        2 - 1550nm"""
+        wave = raw_input("\nEnter the number of your selection: ")
+        if wave not in ('1', '2'):
+            print "\nThat is not a valid selection."
+        if wave == '1':
             wavelength = 1310
         else:
             wavelength = 1550
@@ -157,14 +196,17 @@ def max_cable():
         connector_loss = Decimal('0.5') * connectors
         mode_type = "Multi Mode"
         print """\nWhat is the wavelength being used?
-        1 - 850
-        2 - 1300"""
-        wave = int(raw_input("Enter the number of your selection: "))
-        if wave == 1:
+        1 - 850nm
+        2 - 1300nm"""
+        wave = raw_input("Enter the number of your selection: ")
+        if wave not in ('1', '2'):
+            print "That is not a valid selection."
+            menu()
+        if wave == '1':
             wavelength = 850
         else:
             wavelength = 1300
-    print "\nThe total loss introduced for the %s link by connectors is %s dB\n" % (mode_type, connector_loss)
+    print "\nThe total loss introduced for the %s link by connectors is %sdB\n" % (mode_type, connector_loss)
     print """What is the split ratio of the TAP?
     1 - 50/50
     2 - 60/40
