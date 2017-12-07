0abbdba977056ebd359d27420f20921ebaa7c58f
LinuxFlyGuy <vagrant83@gmail.com>
2017-12-07 11:15:11 -0500

    added recommended split ratios and cleaning up.

--- lightbudget.py
+++ lightbudget.py
@@ -5,7 +5,7 @@
 from decimal import *
 
 def menu():
-    option = raw_input("""What would you like to do:
+    option = raw_input("""\nWhat would you like to do:
     1 - Calculate the max allowed coupler loss for inserting a TAP into a link
     2 - Calculate the max allowed cable length for a given TAPped link
     3 - Exit
@@ -75,11 +75,60 @@ def max_split():
     else:
         print "Something went wrong."
     cable_loss = Decimal(cable / 1000.00000) * attenuation
-    print "\nThe loss introduced by the cable length for the %s %s link is %s dB based on %s dB/km fiber attenuation.\n" % (mode_type, wavelength, cable_loss, attenuation)
+    print """\nThe loss introduced by the cable length for the %s %s link
+    is %s dB based on %s dB/km fiber attenuation.\n""" % (mode_type, wavelength, cable_loss, attenuation)
     total_cable_loss = connector_loss + cable_loss
     print "The total connection loss is %s dB\n" % total_cable_loss
     allowable_loss = link_loss_budget - total_cable_loss
-    print "The allowable coupler loss allowed for a TAP is a %s dB maximum at the monitor port\n" % allowable_loss
+    print """The allowable coupler loss for a TAP is a %s dB
+    maximum at the monitor port\n""" % allowable_loss
+    loss_match(mode_type, allowable_loss)
+
+
+def loss_match(mode, loss):
+    #Maximum recommended values
+    taps_mm = {
+               '50/50': {'Network': '4.5', 'Monitor': '4.5'},
+               '60/40': {'Network': '3.1', 'Monitor': '5.1'},
+               '70/30': {'Network': '2.4', 'Monitor': '6.3'},
+               '80/20': {'Network': '1.8', 'Monitor': '8.1'},
+               '90/10': {'Network': '1.3', 'Monitor': '11.5'}
+               }
+    taps_sm = {
+               '50/50': {'Network': '3.7', 'Monitor': '3.7'},
+               '60/40': {'Network': '2.8', 'Monitor': '4.8'},
+               '70/30': {'Network': '2.0', 'Monitor': '6.1'},
+               '80/20': {'Network': '1.3', 'Monitor': '8.0'},
+               '90/10': {'Network': '0.8', 'Monitor': '12.0'}
+               }
+    #Adjusted average Cubro values
+    # taps_mm = {
+    #            '50/50': {'Network': '4.5', 'Monitor': '4.5'},
+    #            '60/40': {'Network': '3.1', 'Monitor': '5.1'},
+    #            '70/30': {'Network': '2.4', 'Monitor': '6.3'},
+    #            '80/20': {'Network': '1.8', 'Monitor': '8.1'},
+    #            '90/10': {'Network': '1.3', 'Monitor': '11.5'}
+    #            }
+    # taps_sm = {
+    #            '50/50': {'Network': '3.6', 'Monitor': '3.5'},
+    #            '60/40': {'Network': '2.8', 'Monitor': '4.8'},
+    #            '70/30': {'Network': '2.0', 'Monitor': '6.1'},
+    #            '80/20': {'Network': '1.3', 'Monitor': '8.0'},
+    #            '90/10': {'Network': '0.8', 'Monitor': '12.0'}
+    #            }
+    usable = []
+    if mode == 'Single Mode':
+        for split in taps_sm:
+            if float(taps_sm[split]['Monitor']) < float(loss):
+                usable.append(split)
+    elif mode == 'Multi Mode':
+        for split in taps_mm:
+            if float(taps_mm[split]['Monitor']) < float(loss):
+                usable.append(split)
+    else:
+        print "Something went wrong"
+    print """The following split ratios are acceptable for this link
+    %s""" % usable
     menu()
 
 def max_cable():
